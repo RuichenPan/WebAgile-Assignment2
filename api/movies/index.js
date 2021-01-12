c33ff0609488d9214aa128fecda448c87b38d316
Ruichen Pan <3196968196@qq.comm>
2021-01-12 12:29:47 +0000

    modify

--- api/movies/index.js
+++ api/movies/index.js
@@ -3,6 +3,7 @@ import {
   getMovies, getMovie, getMovieReviews
 } from '../tmdb-api';
 import movieModel from './movieModel'
+// import watchlistMovieModel from '../watchlistMovies/watchlistMovieModel'
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -21,4 +22,22 @@ router.get('/:id/reviews', (req, res, next) => {
   .catch((error) => next(error));
 });
 
+// router.post('/:id',async (req, res, next) => {
+//   const id = req.params.id;
+//   const addmovie=await movieModel.findByMovieDBId(id);
+//   if (req.query.action === 'addtowatchlist') {
+//     if(addmovie){
+//       await watchlistMovieModel.collection.insertOne(addmovie).catch(next);
+//       res.status(200).json({
+//       code:200,
+//       message:'success add movie into watch list'
+//       })
+//     }else{
+//       res.status(401).json({
+//       code:401,
+//       message:'Sorry, this movie id is not exist.'
+//     })
+//   }
+// };
+// });
 export default router;
