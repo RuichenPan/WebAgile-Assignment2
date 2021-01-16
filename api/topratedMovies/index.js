b3c8f29ddd1d2d35969b2826ea699c338127afcf
Ruichen Pan <3196968196@qq.comm>
2021-01-16 20:12:57 +0000

    Modify the detailed toprated movies api

--- api/topratedMovies/index.js
+++ api/topratedMovies/index.js
@@ -6,9 +6,14 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   topratedMovieModel.find().then(movies => res.status(200).send(movies)).catch(next);
 });
-router.get('/:id', (req, res, next) => {
-  const id = parseInt(req.params.id);
-  topratedMovieModel.findByMovieDBId(id).then(movie => res.status(200).send(movie)).catch(next);
+router.get('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const movie = await topratedMovieModel.findByMovieDBId(id)
+  if(movie){
+    res.status(200).send(movie)
+  }else{
+    res.status(401).send("Sorry, this movie id is not exist.")
+  }
 });
 
 
